6aa53595742315961be85d15055ed058fc99c598
Nestor Alvarez Diaz <nestor@consensusx.com>
2017-10-04 15:31:12 +0100

    Adding Electron

---
+++ index.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 500,
+        height: 150,
+        title: "Count Down",
+        resizable: false
+    });
+
+    win.setMenu(null);
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
+}
+
+app.on('ready', createWindow);
